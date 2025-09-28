d75d8e7cca1b34fde8d3a6a7598142b27c8f1c57
Harsh <itsmehxrsh@gmail.com>
2025-09-28 14:41:26 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -38,8 +38,8 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=["https://taxwise.harshworks.tech", "http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
